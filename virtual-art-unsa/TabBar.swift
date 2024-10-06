73f18769eaa352508d7717127d9cc6a8d9a88822
atipaq <alex.ronny.hq@gmail.com>
2024-10-06 17:00:24 -0500

    file TabBar added
    Develop ui-design

---
+++ virtual-art-unsa/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  virtual-art-unsa
+//
+//  Created by Alex Huaracha on 6/10/24.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    TabBar()
+}
