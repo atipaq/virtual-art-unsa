326c744a6efc71fe0be300101a75b77dc0b026ea
atipaq <alex.ronny.hq@gmail.com>
2024-10-06 15:52:36 -0500

    Initial Commit

---
+++ virtual-art-unsa/virtual_art_unsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtual_art_unsaApp.swift
+//  virtual-art-unsa
+//
+//  Created by Alex Huaracha on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct virtual_art_unsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
